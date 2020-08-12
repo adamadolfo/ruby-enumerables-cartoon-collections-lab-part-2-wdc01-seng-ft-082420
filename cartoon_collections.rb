578ffaec05f0fe39085be2b86ec2eeefbc20544e
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-12 15:18:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,5 +28,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |string|
     valid_calls.include? string
-    
+  end
 end
